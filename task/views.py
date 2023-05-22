e987407cfe3e5bcabc51b643f64a0275aa2debbf
[jeison-21] <[jeisonfallas2198@gmail.com]>
2023-05-22 12:31:47 -0600

    inicio del proyecto

---
+++ task/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+    
+def signup(request):
+    return render(request, 'signup.html',{
+        'form': UserCreationForm
+    })
+
